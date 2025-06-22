aba9590339727c267b3dd491b81be35e03c740cc
vyshnavi <vishuchowdary20@gmail.com>
2025-06-22 14:04:09 +0530

    saved dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import react ,{useState,useEffect}from 'react';
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const[data,setData] =useState([]);
   const [perpage,setPerpage] = useState([]);
+
+  //useeffect to call api 
+  //copied url from jsonpaceholder website
+
+  useEffect(()=>{
+
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(
+
+      resizeBy => {setData(resizeBy.data)}
+    )
+  })
  return(
   <div className='App'>
     Helloworld
