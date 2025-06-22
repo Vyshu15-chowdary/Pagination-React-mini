aef28ca60606e3bc8914f6d596024bce6d36a580
vyshnavi <vishuchowdary20@gmail.com>
2025-06-22 14:29:31 +0530

    Mapping of pages done

--- src/Pagination.js
+++ src/Pagination.js
@@ -4,7 +4,9 @@ const Pagination = () => {
     let pageNumbers = [1,2,3,4,5,6,7,8,9,10]
   return (
     <div>
-
+        <center>
+            {pageNumbers.map(page => <div className='pagebutton'>{page}</div>)}
+        </center>
     </div>
   )
 }
